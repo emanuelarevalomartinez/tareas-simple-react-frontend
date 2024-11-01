4a154f41da9b607c0b7ab4f73fb44093057d3396
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2024-11-01 02:05:09 -0400

    correccionnnn

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/disttareas-simple-react-frontend/',
+  base: '/tareas-simple-react-frontend/',
 })
