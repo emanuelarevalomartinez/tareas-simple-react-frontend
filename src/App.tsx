3824c7c92335ba1c43521d34db5a3ea3bfee05d4
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2024-11-01 02:37:58 -0400

    otro par de cambios visuales finales

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { TareaContextoGlobal } from "./context"
 function App() {
 
   return (
-   <div className="bg-zinc-900 h-screen text-white flex justify-center items-start sm:items-center">
-    <div className="bg-zinc-950 p-4 w-full sm:w-2/5 ">
+   <div className="bg-zinc-900 min-h-screen text-white flex justify-center items-start">
+    <div className="bg-zinc-950 p-4 min-h-screen sm:min-h-min w-full sm:w-2/5 ">
        <h1 className="text-3xl font-bold text-center block my-2">App de Tareas</h1>
        <TareaContextoGlobal>
          <TareaFormulario/>
